92bb4138e99f8bfbd44e0a5ceb554f8d9b28e932
alulab14 <alulab14@inf.pucp.edu.pe>
2019-10-09 19:48:27 -0500

    UsuarioMYSQL

---
+++ LP2TECNOQUIMCONTROLLER/src/lp2tecnoquim/mysql/UsuarioMySQL.java
@@ -0,0 +1,98 @@
+package lp2tecnoquim.mysql;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import lp2tecnoquim.config.DBManager;
+import lp2tecnoquim.dao.UsuarioDAO;
+import lp2tecnoquim.model.Usuario;
+
+public class UsuarioMySQL implements UsuarioDAO {
+
+    Connection con = null;
+    CallableStatement cs;
+    
+    @Override
+    public void insertar(Usuario usuario) {
+        try{
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call INSERTAR_USUARIO(?,?,?)}");
+            cs.setString("_USERNAME ", usuario.getUsername());
+            cs.setString("_CONTRASENA ",usuario.getPassword());
+            
+            cs.registerOutParameter("_ID_USUARIO ", java.sql.Types.INTEGER);
+            cs.executeUpdate();
+            usuario.setIdUsuario(cs.getInt("_ID_USUARIO "));
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+    }
+
+    @Override
+    public void actualizar(Usuario usuario) {
+        try{
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call ACTUALIZAR_ALMACEN(?,?,?)}");
+            cs.setInt("_ID_USUARIO", usuario.getIdUsuario());
+            cs.setString("_USERNAME", usuario.getUsername());
+            cs.setString("_CONTRASENA",usuario.getPassword());
+                    
+            cs.executeUpdate();
+            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+    }
+
+    @Override
+    public void eliminar(int idUsuario) {
+        try{
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call ELIMINAR_ALMACEN(?)}");
+            cs.setInt("_ID_USUARIO", idUsuario);
+            
+           
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+    }
+
+    @Override
+    public ArrayList<Usuario> listar() {
+        ArrayList<Usuario> usuario = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call LISTAR_ALMACEN()}");
+            ResultSet rs = cs.executeQuery();
+            while(rs.next()){
+                Usuario  a = new Usuario();
+                a.setIdUsuario(rs.getInt("ID_USUARIO "));
+                a.setUsername(rs.getString("USERNAME"));
+                a.setPassword(rs.getString("CONTRASENA"));
+                ///////////////////////////////////////////////
+                
+                
+                
+                
+                
+                usuario.add(a);
+            }
+        }catch(ClassNotFoundException | SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(SQLException ex){System.out.println(ex.getMessage());}
+        }
+        return usuario;
+    }
+    
+}
